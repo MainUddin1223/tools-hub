6c2d6820dc9278a919feb2aba2319e6ece810771
MainUddin1223 <2021md.main@gmail.com>
2022-05-24 01:22:16 +0600

    admin role done

--- src/Pages/Dashboard/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin.js
@@ -1,23 +1,46 @@
 import React, { useRef, useState } from 'react';
 import { useQuery } from 'react-query';
+import { toast } from 'react-toastify';
 
 const MakeAdmin = () => {
     const [email, setEmail] = useState('')
     const userEmail = useRef();
-    const role = 'admin';
-    const url = `http://localhost:5000/user/${email}`;
-    const currentUser = { role: role };
+    const [adminData, setAdminData] = useState({});
+    const url = `http://localhost:5000/users/${email}`;
     const getEmail = (event) => {
         event.preventDefault();
         const getEmail = userEmail.current.value
-        setEmail(getEmail);
+        setEmail(getEmail)
+    }
+    if (email) {
         fetch(url)
             .then(res => res.json())
             .then(data => {
-                console.log(data);
+                setAdminData(data)
             })
     }
+    console.log(adminData);
 
+    const makeAdmin = () => {
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+        })
+            .then(res => res.json())
+            .then(data => {
+                toast.success('Successfully add to admin panel', {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+            })
+    }
     // const { data: user, isErrorloading } = useQuery('user', () => fetch(url).then(res => res.json()));
     // console.log(user);
     // if (email) {
@@ -35,8 +58,15 @@ const MakeAdmin = () => {
     // }
     return (
         <div>
+            {
+                adminData !== '' && <div>
+                    <p>{adminData?.displayName}</p>
+                    <p>{adminData?.email}</p>
+                    <button onClick={makeAdmin}>make Admin</button>
+                </div>
+            }
             <input type="email" ref={userEmail} />
-            <button onClick={getEmail}>make admin</button>
+            <button onClick={getEmail}>Add an Admin</button>
         </div>
     );
 };
